de702fc5fce8b106d8aa67fb10a449b391d695a6
Gabriela <gabrielapereirar@hotmail.com>
2023-06-05 18:01:12 -0300

    05 de junho

--- conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //CONFIGURAÇÃO GERAL
-    $servidor="localhost";
-    $usuario="root";
-    $senha="";
-    $banco="dados_abertos";
-
-    $pdo = new PDO ("mysql:host=$servidor;dbname=$banco,$usuario,$senha");
-?>
